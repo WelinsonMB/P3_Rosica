ecc57b44bd95e8f710fbdc3d0e1d6047c4b72b93
WelinsonMB <welinson.mendes2015@hotmail.com>
2024-11-19 17:39:11 -0300

    Alteração das funções

--- Programa_menu_alterado.c
+++ Programa_menu_alterado.c
@@ -1,107 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h> //Biblioteca feita para realizar calculos matemticos como raizes 
 
-#define PI 3.14159265          // Constante para PI
+#define PI 3.14159265                    // Constante para PI
+#define RAIZ_3 1.732050808              // Constante da Raiz de 3
+#define METROS_PARA_JARDAS 1.09361      // Conversão metros para jardas
+#define CMCUB_PARA_POLCUB 16.387064   // Conversão centimetros cubicos para polegadas cubicas
+#define LITROS_PARA_GALOES 0.2642       // Conversão litros para galões
+#define KG_PARA_LIBRAS 2.20462262       // Conversão quilos para libras
+
+ //Funcao para calcular a area  de figuras planas 
+float area_figura(int tipo, float w1, float w2, float w3){
+
+      switch (tipo)
+      {
+      case 1: return PI * (w1/2) * (w1/2); //Circulo (w1 é o diâmetro), logo a formula é pi vezes raio ao quadrado.
+      case 2: return (w1 * w2) / 2; //Losango (w1 e w2 são as diagonais)
+      case 3: return w1 * w2; //Paralelogramo (w1 é a base e w2 é altura (base * altura))
+      case 4: return ((w1 + w2)/2)*w3; //trapezio (w1 é a base maior, w2 é a base menor e w3 é altura)
+      case 5: return ((w1 * w1) / 4) * RAIZ_3; // triangulo quilatero (w1 é a diagonal)
+      case 6: return (w1 * w2) /2; //Triangulo  (w1 é a base, w2 é altura)
+
+      default: return 0;
+      }
+}
+
+//Função para calcular o volume do solidos geometricos
+
+float volume_solido(int tipo, float w1, float w2, float w3){
+
+      switch (tipo)
+      {
+      case 1: return ((4 * PI) / 3)*(w1/2)*(w1/2)*(w1/2); //Esfera onde w1 é o diametro, e operação (w1/2) é para descobrir o raio
+      case 2: return ((PI/3)*(w1/2)*(w1/2)*w2); //Cone, onde w1 é o diametro e w2 é altura
+      case 3: return (PI * (w1/2)* (w1/2) * w2); //cilintro onde w1 é o diametro e w2 é altura
+      case 4: return (w1 * w2 * w3 ); //Paralelepipedo  onde w1,w2 e w3 são base, largura e altura
+
+      default: return 0;
+            break;
+      }
+
+}
+
+float conversao(int tipo, float num){
+      switch (tipo){
+      case 1: return num * METROS_PARA_JARDAS;
+      case 2: return num / METROS_PARA_JARDAS; // Nesta linha está dividindo pois é de Jardas para metros 
+      case 3: return num / CMCUB_PARA_POLCUB; // Centimetros cubicos para polegadas cubicas
+      case 4: return num * CMCUB_PARA_POLCUB; //Polegadas cubicas para centimetros cubicos
+      case 5: return num * LITROS_PARA_GALOES;
+      case 6: return num / LITROS_PARA_GALOES; //Galoes para litroes
+      case 7: return num * KG_PARA_LIBRAS; 
+      case 8: return num / KG_PARA_LIBRAS; //Libras para Kg 
+      default: return 0; 
+            
+      }
+}
 
 
-
-
-
-
-
-float circulo(float dia)  //fun��o que calcula a �rea do c�rculo 
-      {float r, area;
-      r = dia/2;
-      area = PI*r*r;
-      return(area);}
-
-float losango(float D1,float D2) //fun��o que calcula a area do losango
-      {float area;
-      area=(D1*D2)/2;
-      return(area);}
-
-float paralelogramo(float alt, float base)    // fun��o que calcula a area do paralelogramo
-      {float area;
-      area=base*alt;
-      return(area);}
-     
-float trapezio(float B_maior, float b_menor,float alt)  //fun��o que calcula a area do trap�zio
-      {float area;
-      area=((B_maior+b_menor)/2)*alt;
-      return(area);}
-
-float tri_equi(float A, float B, float C)           //fun��o que calcula a area do triangulo equilatero
-      {float area;
-      area= ((A*A)/4)*1.732050808;
-      return (area);}
-       
-float tri(float A, float B, float C)          //fun��o que calcula a area do triangulo
-      {float area;
-      area=(B*A)/2;
-      return(area);}
-
-float esf(float dia)                     //fun��o que calcula o volume da esfera
-      {float r, vol;
-      r=dia/2;
-      vol=((4*3.14159265)/3)*r*r*r;
-      return (vol);}
       
-float cone(float dia, float alt)           //fun��o que calcula o volume do cone
-      {float r, vol;
-      r=dia/2;
-      vol=(3.14159265/3)*r*r*alt;
-      return(vol);}
-      
-float cilindro(float dia, float alt)       //fun��o que calcula o volume do cilindro
-      {float r, vol;
-      r=dia/2;
-      vol=3.14159265*r*r*alt;
-      return(vol);}
 
-float paral(float comp, float larg, float alt)          //fun��o que calcula o volume do paralelep�pedo
-      {float vol;
-      vol=comp*larg*alt;
-      return(vol);}
-
-float m_j(float num)                    //fun��o que converte metros para jardas
-      {float total;
-      total= num*1.09361;
-      return (total);}
-                   
-float j_m(float num)                    //fun��o que converte jardas para metros
-      {float total;
-      total= num/1.09361;
-      return(total);}
-
-float cmcub_polcub(float num)         //fun��o que converte cent. cub. para pol cub
-      {float total;
-      total=num/16.387064;  
-      return(total);}
-
-float polcub_cmcub(float num)        //fun��o que converte pol cub. para cent. cub
-      {float total;
-      total= num*16.387064; 
-      return(total);}
-
-float l_gl(float num)              //fun��o que converte litros em galoes
-      {float total;
-      total= num*0.2642;
-      return(total);}
-
-float gl_l(float num)              //fun��o que converte galoes em litros
-      {float total;
-      total= num/0.2642;
-      return(total);}
-
-float kg_lib(float num)           //fun��o que converte quilos em libras
-      {float total;
-      total= num*2.20462262;
-      return(total);}
-
-float lib_kg(float num)         //fun��o que converte libras em quilos
-      {float total;
-      total= num/2.20462262;
-      return(total);}     
       
 main()
 {
